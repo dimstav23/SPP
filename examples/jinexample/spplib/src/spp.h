009fd913453fb7813b622de9edfa27400a59bc55
alexandrinapanfil <44653552+alexandrinapanfil@users.noreply.github.com>
2021-12-16 10:12:38 +0100

    Add GEP instrumentation (with empty rtfunction))

---
+++ examples/jinexample/spplib/src/spp.h
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////
+//////          spp.h           ///////////////
+///////////////////////////////////////////////
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef SPP_HEADER_H_
+#define SPP_HEADER_H_
+
+///////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <math.h>
+
+#define __SPP_MASK_TAG_OUT 0xFFFFFFFFFFFF
+
+extern void * __SPP_clean_tag (void * ptr);
+
+extern void* __spp_updatetag(void* ptr, int64_t off);
+
+///////////////////////////////////////////////
+
+#endif // SPP_HEADER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
