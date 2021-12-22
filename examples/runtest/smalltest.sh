66a9a8578e4b7631a0a128e7a760c1b16d750ba9
poolmoo <myoungjin.nam@gmail.com>
2021-12-22 14:17:55 +0100

    Wrappers for free, string functions are added. They are interposed at link time. check /runtest/smalltest.sh and wrappers_spp.c
    TODO: Those functions should be skipped intrumenting
    in llvm transformation time.

--- examples/runtest/smalltest.sh
+++ examples/runtest/smalltest.sh
@@ -5,8 +5,8 @@
 ##   NOTE: SET LLVMROOT PATH BELOW!  ##
 #######################################
 
-LLVMROOT=/PATH/TO/CLANGLLVM/INSTALL
-#LLVMROOT=~/.local/spp.llvm.12.0.0/
+#LLVMROOT=/PATH/TO/CLANGLLVM/INSTALL
+LLVMROOT=~/.local/spp.llvm.12.0.0/
 
 CLANG="${LLVMROOT}/bin/clang"
 OPTLEV="-O2"
@@ -19,14 +19,27 @@ SPPLIB="${SPPROOT}/examples/smallexample/spplib/"
 SPPLIBSRC="${SPPLIB}/src/"
 SPPLIBOBJ="${SPPLIB}/obj/"
 
-# Compile spp hook functions ###################
+WRAP_LIST="-Xlinker --wrap=free -Xlinker --wrap=strcpy -Xlinker --wrap=strcmp -Xlinker --wrap=strncpy -Xlinker --wrap=strncmp -Xlinker --wrap=memcmp -Xlinker --wrap=memchr -Xlinker --wrap=strchr -Xlinker --wrap=strncat -Xlinker --wrap=strtol -Xlinker --wrap=strlen -Xlinker --wrap=strchrnul"   
+
+
+#   Compile spp hook functions      ###################
 
 rm "${SPPLIBOBJ}/spp_hookobj.o"
+rm "${SPPLIBOBJ}/wrappers.o"
+
+# compiling spp.c
 
 $CLANG -emit-llvm \
 "${SPPLIBSRC}/spp.c" \
 -c -o \
 "${SPPLIBOBJ}/spp_hookobj.o"
+
+# compiling wrappers.c
+
+$CLANG \
+"${SPPLIBSRC}/wrappers_spp.c" \
+-c -o \
+"${SPPLIBOBJ}/wrappers.o" 
 
 echo "----- Hook functions compiled ----------"
 
@@ -37,9 +50,11 @@ echo "----- Hook functions compiled ----------"
 $CLANG \
 $OPTLEV \
 -flto \
+-fuse-ld=/usr/bin/ld.gold \
 -Xclang -load -Xclang "${LLVMROOT}/lib/LLVMSPP.so"  \
 -include "${SPPLIBSRC}/spp.h" \
 "-I${PMDKSRC}/include/" "-L${PMDKSRC}/debug/" \
+$WRAP_LIST "${SPPLIBOBJ}/wrappers.o" \
 -Xlinker "${SPPLIBOBJ}/spp_hookobj.o" \
 -DTAG_BITS=15 -lpmem -lpmemobj \
 "${TESTSRC}/mymalloc.c" "${TESTSRC}/myfree.c" "${TESTSRC}/mystrcpy.c" "${TESTSRC}/main.c" \
