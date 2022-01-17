7ab2eedb647665e3d166d7ce12beb371babd3d8f
dimstav23 <dimstav23@gmail.com>
2022-01-17 11:37:47 +0000

    re-add default.nix

---
+++ default.nix
@@ -0,0 +1,37 @@
+# we ping nixpkgs soureces here according to https://nix.dev/tutorials/towards-reproducibility-pinning-nixpkgs
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3c52ea8c9216a0d5b7a7b4d74a9d2e858b06df5c.tar.gz") {}
+}:
+pkgs.mkShell {
+  # find packages here: https://search.nixos.org/packages
+  # add libraries here
+  buildInputs = [
+    pkgs.libndctl
+    pkgs.glib
+  ];
+  # add tools here
+  nativeBuildInputs = [
+    pkgs.pkg-config
+    pkgs.pandoc
+    pkgs.m4
+    pkgs.bashInteractive
+    pkgs.cmake
+    pkgs.dos2unix
+    pkgs.binutils-unwrapped
+    #(pkgs.callPackage ./impure-clang.nix {})
+  ];
+
+  shellHook = ''
+    export CMAKE_C_COMPILER=${pkgs.gcc}/bin/gcc
+    export CC=${pkgs.gcc}/bin/gcc
+    export CMAKE_CXX_COMPILER=${pkgs.gcc}/bin/g++
+    export CXX=${pkgs.gcc}/bin/g++
+  '';
+
+  ## Disable mapping address randomization and provide PMDK with appropriately low mapping address hint:
+  PMEM_MMAP_HINT = 0;
+
+  ## If your machine is not equipped with PM, export the following flag to use flush instructions instead of msync:
+  PMEM_IS_PMEM_FORCE = 1;
+
+}
+
