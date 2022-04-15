25a4b4dcc9201786d9c6341cd159452dc5d7def4
dimstav23 <dimstav23@gmail.com>
2022-04-15 12:40:15 +0000

    add plot utilities and starting documentation

---
+++ plot_utils/import_pmemkv.sh
@@ -0,0 +1,46 @@
+set -e
+
+# declare -a variants_plotter=("pmdk" "safepm" "spp")
+# declare -a variants_results=("vanilla" "safepm" "spp")
+
+declare -a variants_plotter=("pmdk" "spp")
+declare -a variants_results=("vanilla_pmdk" "spp")
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: import_pmemkv.sh <results location>" 2>&1
+  exit 1
+fi
+
+for (( i=0; i<${#variants_plotter[@]}; i++))
+do
+  variant_plotter="${variants_plotter[i]}"
+  mkdir -p "$variant_plotter"
+  variant_results="${variants_results[i]}"
+  if [ -e ${variant_plotter}/pmemkv_result ]
+  then
+    j=1
+    while true
+    do
+      if ! [ -e ${variant_plotter}/pmemkv_result_${j} ]
+      then
+        break
+      fi
+      j=$((j+1))
+    done
+    
+    mv ${variant_plotter}/pmemkv_result ${variant_plotter}/pmemkv_result_${j}
+  fi
+  echo "delete_random: delete_random [6] [group: pmemobj]" > ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/delete/deleterandom_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+  echo "delete_seq: delete_seq [6] [group: pmemobj]" >> ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/delete/deleteseq_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+  echo "randomreadwrite50: randomreadwrite50 [6] [group: pmemobj]" >> ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/randomreadwrite/50_readpercentage/readrandomwriterandom_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+  echo "randomreadwrite95: randomreadwrite95 [6] [group: pmemobj]" >> ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/randomreadwrite/95_readpercentage/readrandomwriterandom_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+  echo "read_random: read_random [6] [group: pmemobj]" >> ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/read/readrandom_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+  echo "read_seq: read_seq [6] [group: pmemobj]" >> ${variant_plotter}/pmemkv_result
+  cat $1/${variant_results}/read/readseq_threads.csv | sed 's/,/;/g' >> ${variant_plotter}/pmemkv_result
+done
