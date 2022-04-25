66c60f74de5cea14a8f73df1aafd8775cbad70be
dimstav23 <dimstav23@gmail.com>
2022-04-25 18:16:35 +0000

    create safepm docker image -- fix in vanilla llvm to include compiler-rt

--- utils/docker/compiler_images/install_llvm.sh
+++ utils/docker/compiler_images/install_llvm.sh
@@ -6,7 +6,7 @@ cd /llvm-project/build
 cmake -G "Unix Makefiles" \
 -DCMAKE_BUILD_TYPE=Release \
 -DLLVM_BINUTILS_INCDIR=/usr/include \
--DLLVM_ENABLE_PROJECTS="clang" \
+-DLLVM_ENABLE_PROJECTS="clang;compiler-rt" \
 -DLLVM_TARGETS_TO_BUILD=X86 \
 -DCMAKE_INSTALL_PREFIX=/llvm ../llvm
 
---
+++ utils/docker/packaged_environments/create_env_safepm.sh
@@ -0,0 +1,2 @@
+set -e
+docker build -t safepm -f=Dockerfile_safepm .
