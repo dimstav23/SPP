28be543b90a04d3f61bd0ca7d9617444ef902c41
dimstav23 <dimstav23@gmail.com>
2022-04-25 18:17:33 +0000

    inluce safepm in the pmembench variants

--- benchmarks/pmembench/run_variants.sh
+++ benchmarks/pmembench/run_variants.sh
@@ -12,3 +12,5 @@ mkdir -p results/pmembench_tx
 docker run -v "$BENCHMARK_PM_PATH:/mnt/pmembench" -v "$(pwd)/results":/results -v "$(pwd)/run_spp.sh":/run_spp.sh -t spp bash /run_spp.sh
 
 docker run -v "$BENCHMARK_PM_PATH:/mnt/pmembench" -v "$(pwd)/results":/results -v "$(pwd)/run_vanilla.sh":/run_vanilla.sh -t vanilla_pmdk bash /run_vanilla.sh
+
+docker run -v "$BENCHMARK_PM_PATH:/mnt/pmembench" -v "$(pwd)/results":/results -v "$(pwd)/run_safepm.sh":/run_safepm.sh -t safepm bash /run_safepm.sh
