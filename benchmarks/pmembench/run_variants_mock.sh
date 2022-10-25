f5587102c6979a77af058365a141523484f7d96d
dimstav23 <dimstav23@gmail.com>
2022-10-25 14:50:00 +0000

    add automated scripts for running the mocked spp variants

---
+++ benchmarks/pmembench/run_variants_mock.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if ! [ -d "$BENCHMARK_PM_PATH" ]
+then
+  echo "Please set BENCHMARK_PM_PATH to a valid directory." 2>&1
+  exit 1
+fi
+
+if [[ $(lscpu | grep "NUMA node(s)" | awk '{print $3}') > 1 ]]
+then
+  if [ -z "$NUMA_NODE" ]
+  then
+    echo "The current system has multiple NUMA nodes." 2>&1
+    echo "Please set NUMA_NODE to the node where the mounted PM in $BENCHMARK_PM_PATH is attached." 2>&1
+    echo "e.g. NUMA_NODE=0"
+    exit 1
+  fi
+  NUMA_CPU_CORES="$(lscpu | grep "NUMA node$NUMA_NODE" | awk '{print $4}')"
+  echo "CPU Cores to pin the benchmarks: $NUMA_CPU_CORES"
+fi
+
+mkdir -p results/pmembench_map
+mkdir -p results/pmembench_tx
+
+docker run -v "$BENCHMARK_PM_PATH:/mnt/pmembench" -v "$(pwd)/results":/results -v "$(pwd)/run_spp.sh":/run_spp.sh -e NUMA_CPU_CORES=$NUMA_CPU_CORES -t spp_mock bash /run_spp.sh
