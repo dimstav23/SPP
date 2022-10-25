57344844414c2044ede6e404b21d010386de5f7c
dimstav23 <dimstav23@gmail.com>
2022-10-25 11:11:39 +0000

    ripe fixes for O1 & O2 automation

--- benchmarks/ripe/run-variants_O1.sh
+++ benchmarks/ripe/run-variants_O1.sh
@@ -31,10 +31,10 @@ do
   then
     # We trick valgrind into not producing (large) core dumps by creating a read-only directory and running the tests in that. But for that to work, we must run as a non-root user in the container
     docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" ripe64-$variant rm -f /mnt/ripe/vmem_dummy.pool # The non-root user in the container cannot overwrite the pool created by the root user during the previous runs
-    docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -v "$(pwd)/results:/results" -t --shm-size=2g --user spp_user ripe64-$variant ./ripe_tester.py both 3 gcc -o /spp-pass/benchmarks/ripe/results/$variant
+    docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -v "$(pwd)/results:/spp-pass/benchmarks/ripe/results" -t --shm-size=2g --user spp_user ripe64-$variant ./ripe_tester.py both 3 clang -o /spp-pass/benchmarks/ripe/results/$variant
   elif [ "$variant" = "safepm" ]
   then
-    docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -v "$(pwd)/results:/spp-pass/benchmarks/ripe/results" -t --shm-size=2g ripe64-$variant ./ripe_tester.py both 3 gcc -o /spp-pass/benchmarks/ripe/results/$variant
+    docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -v "$(pwd)/results:/spp-pass/benchmarks/ripe/results" -t --shm-size=2g ripe64-$variant ./ripe_tester.py both 3 clang -o /spp-pass/benchmarks/ripe/results/$variant
   else
 	  docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -v "$(pwd)/results:/spp-pass/benchmarks/ripe/results" -t --shm-size=2g ripe64-$variant ./ripe_tester.py both 3 clang -o /spp-pass/benchmarks/ripe/results/$variant
     #docker run -v "$BENCHMARK_PM_PATH:/mnt/ripe" -t --shm-size=2g -it ripe64-$variant bash
